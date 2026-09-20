9a26c4d82230ad5031b1cda5bde333f274fb57ba
Filipe Joana <filipejanny@hotmail.com>
2026-09-20 23:59:37 +0100

    Th

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const yearNode = document.querySelector('[data-year]');
+
+  if (yearNode) {
+    yearNode.textContent = new Date().getFullYear();
+  }
+});
